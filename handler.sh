a5963a15d7ab292fc6183b6d7baf11b80d0cc33c
limed <limed@mozilla.com>
2019-05-15 15:25:18 -0700

    Renaming some variables

--- handler.sh
+++ handler.sh
@@ -3,9 +3,11 @@
 set -u
 set -x
 
-HOSTNAME=$(curl -fsq http://169.254.169.254/latest/meta-data/hostname)
+if [ -z "${NODENAME}" ]; then
+    NODENAME=$(curl -fsq http://169.254.169.254/latest/meta-data/hostname)
+fi
 
-kubectl cordon "${HOSTNAME}"
-kubectl drain "${HOSTNAME}" --ignore-daemonsets --grace-period=30 --force
+kubectl cordon "${NODENAME}"
+kubectl drain "${NODENAME}" --ignore-daemonsets --grace-period=30 --force
 
 sleep 10
